ec534765bbb99c1d452e5225e85ca332345879c0
varmaxon <v.varmax@yandex.ru>
2022-10-05 00:04:29 +0300

    start of the changing levels

--- scripts/work_with_hexagons.js
+++ scripts/work_with_hexagons.js
@@ -10,14 +10,26 @@ class hexagon {
 hexagonElements = document.querySelectorAll(".one-hexagon");
 clickedHexagons = [];
 
+levels = ['red', 'green', 'blue', 'orange', 'black']
+formulas = ['1', '2', '3', '4', '5']
+
 for (hex of hexagonElements) {
 	hex.addEventListener("click", function() {
-		this.style.opacity = 0.7;
+		this.style.opacity = 0.6;
+		this.style.border = 3;
 		//remember all the data of the clicked element
 
-		alert(this.querySelector(".inside").querySelector(".name").textContent);
-		alert(this.querySelector(".inside").querySelector(".MLT").dataset.M)
+		// alert(this.querySelector(".inside").querySelector(".name").textContent);
+		// alert(this.querySelector(".inside").querySelector(".MLT").dataset.M)
 		// var newHex = new hexagon("alala");
 		// alert(newHex.name)
+	})
+	var cnt = 0
+	hex.addEventListener("contextmenu", function() {
+		this.style.color = levels[cnt];
+		// this.style.
+		// this.firstChild.nodeValue = "New Text";
+		cnt++;
+		if (cnt == levels.length) cnt = 0;
 	})
 }
