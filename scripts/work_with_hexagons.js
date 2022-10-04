42801d580c4b2b5a6cacfd35cd4bac723f2808a8
liubavabarsegian <lyubahemmo@mail.ru>
2022-10-04 21:57:22 +0300

    minor changes

--- scripts/work_with_hexagons.js
+++ scripts/work_with_hexagons.js
@@ -1,12 +1,23 @@
-import {hexagon} from "./HexClass.js"
+class hexagon {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
 
 hexagonElements = document.querySelectorAll(".one-hexagon");
 clickedHexagons = [];
 
 for (hex of hexagonElements) {
-	hex.addEventListener("click", function(){
+	hex.addEventListener("click", function() {
 		this.style.opacity = 0.7;
 		//remember all the data of the clicked element
-		var newHex = new hexagon("alala");
+
+		alert(this.querySelector(".inside").querySelector(".name").textContent);
+		alert(this.querySelector(".inside").querySelector(".MLT").dataset.M)
+		// var newHex = new hexagon("alala");
+		// alert(newHex.name)
 	})
 }
