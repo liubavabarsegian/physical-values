2315298d3cc434c52ac076c9eb0061c07bd0bee9
liubavabarsegian <lyubahemmo@mail.ru>
2022-11-27 18:40:58 +0300

    screen attempts

---
+++ scripts/screen.js
@@ -0,0 +1,29 @@
+//const html2canvas = require('window.html2canvas');
+//import html2canvas from 'window.html2canvas';
+
+function takeshot() {
+	let body =
+		document.querySelector("body");
+
+	// Use the html2canvas
+	// function to take a screenshot
+	// and append it
+	// to the output div
+
+	//так работает
+
+	// html2canvas(body).then(
+	// 	function (canvas) {
+	// 		document
+	// 		.querySelector("body")
+	// 		.appendChild(canvas);
+	// 	})
+
+	
+		html2canvas(document.querySelector('body'), {
+            onrendered: function(canvas) {
+                var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+				window.location.href=image;
+            }
+        });
+}
