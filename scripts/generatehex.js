8ff92c2ee1c111c47fdf420b513354fd08548691
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-02-01 14:17:36 +0300

    spacing generahex.js

--- scripts/generatehex.js
+++ scripts/generatehex.js
@@ -2,57 +2,50 @@ sortGK(data)
 createTable("newf", data)
 
 
-
-
 let ContextElement
 function addHexEventListeners() {
   menuArea = document.querySelectorAll(".one-hexagon");
 
 	for (hex of menuArea) {
-    //hex.hidden = false;
 		hex.addEventListener("contextmenu", function(event) {
-				event.preventDefault();
-      	menu.style.top = `${event.clientY}px`;
-				menu.style.left = `${event.clientX}px`;
-				menu.classList.add("active");
-				gkmenu.classList.add("active");
-				gkmenu.style.top = `${event.clientY}px`;
-				gkmenu.style.left = `${event.clientX+175}px`;
-				ContextElement = event
-				hex = getMainHexFromSiblings(ContextElement.target)
-				if (hex == undefined) {return}
+			event.preventDefault();
+            menu.style.top = `${event.clientY}px`;
+			menu.style.left = `${event.clientX}px`;
+			menu.classList.add("active");
+			gkmenu.classList.add("active");
+			gkmenu.style.top = `${event.clientY}px`;
+			gkmenu.style.left = `${event.clientX+175}px`;
+			ContextElement = event
+			hex = getMainHexFromSiblings(ContextElement.target)
+			if (hex == undefined) {return}
 
-        hexData = findHex(hex.id)
-        gkmenu.innerHTML = ""
+            hexData = findHex(hex.id)
+            gkmenu.innerHTML = ""
 
-        hexData.forEach(function (gk,index) {
-          gk = getNGK(hexData,index)
-          if (gk.name != "" && gk.name != getNGK(hexData,0).name) {
-            let gkLiMenu = document.createElement("li")
-            gkLiMenu.classList.add("context-menu__item")
-            let gkAMenu = document.createElement("a")
-            gkAMenu.classList.add("context-menu__link")
-            gkAMenu.innerHTML = gk.name
-            gkLiMenu.appendChild(gkAMenu)
-              gkLiMenu.addEventListener("click", function () {
-
-              
-              swap = hexData[findGKIndex(hexData,gk.GK)]
-              // console.log(findGKIndex(hexData,gk.GK))
-              hexData[findGKIndex(hexData,gk.GK)] = hexData[0]
-              hexData[0] = swap
+            hexData.forEach(function (gk,index) {
+                gk = getNGK(hexData,index)
+                if (gk.name != "" && gk.name != getNGK(hexData,0).name) {
+                    let gkLiMenu = document.createElement("li")
+                    gkLiMenu.classList.add("context-menu__item")
+                    let gkAMenu = document.createElement("a")
+                    gkAMenu.classList.add("context-menu__link")
+                    gkAMenu.innerHTML = gk.name
+                    gkLiMenu.appendChild(gkAMenu)
+                    gkLiMenu.addEventListener("click", function () {   
+                        swap = hexData[findGKIndex(hexData,gk.GK)]
+                        // console.log(findGKIndex(hexData,gk.GK))
+                        hexData[findGKIndex(hexData,gk.GK)] = hexData[0]
+                        hexData[0] = swap
 
 
-                createTable("newf", data);
-                localStorage.setItem('testObject', JSON.stringify(data));
-                undoableCounter.setValue(data);
-            })
-              gkmenu.appendChild(gkLiMenu);
-          }
+                        createTable("newf", data);
+                        localStorage.setItem('testObject', JSON.stringify(data));
+                        undoableCounter.setValue(data);
+                    })
+                    gkmenu.appendChild(gkLiMenu);
+                }
           
-        })
-
-
+            })
             if (hex.classList.contains("invisible")) {
                 document.getElementById("l0").style.display = "none"
 					document.getElementById("l1a").innerHTML = "Добавить"
@@ -63,66 +56,58 @@ function addHexEventListeners() {
 					document.getElementById("l1a").innerHTML = "Редактировать"
 					document.getElementById("l2").style.display = ""
 			}
-      if (checkAnyGKExistence(getHexFullData(hex)) && getHexData(hex).name != "") {
+            if (checkAnyGKExistence(getHexFullData(hex)) && getHexData(hex).name != "") {
 
-          let li = document.createElement("li");
-          li.classList.add("context-menu__item");
-          let a = document.createElement("a");
-          a.classList.add("context-menu__link");
-          a.style.backgroundColor = '#000000';
-          a.style.color = '#FFFFFF';
-          a.innerHTML = "Скрыть 👁";
+                let li = document.createElement("li");
+                li.classList.add("context-menu__item");
+                let a = document.createElement("a");
+                a.classList.add("context-menu__link");
+                a.style.backgroundColor = '#000000';
+                a.style.color = '#FFFFFF';
+                a.innerHTML = "Скрыть 👁";
 
-        a.addEventListener('click', function() {
-          emptyIndex = getEmptyGKIndex(hexData)
-          // hexGK[Object.keys(hexGK)[0]].name
-          let swap = data[hex.parentElement.id][hex.id][0]
-          data[hex.parentElement.id][hex.id][0] = data[hex.parentElement.id][hex.id][emptyIndex]
-          data[hex.parentElement.id][hex.id][emptyIndex] = swap
-          createTable("newf", data)
-          undoableCounter.setValue(data)
-        })
+                a.addEventListener('click', function() {
+                    emptyIndex = getEmptyGKIndex(hexData)
+                    // hexGK[Object.keys(hexGK)[0]].name
+                    let swap = data[hex.parentElement.id][hex.id][0]
+                    data[hex.parentElement.id][hex.id][0] = data[hex.parentElement.id][hex.id][emptyIndex]
+                    data[hex.parentElement.id][hex.id][emptyIndex] = swap
+                    createTable("newf", data)
+                    undoableCounter.setValue(data)
+                })
 
-        li.appendChild(a)
-        gkmenu.appendChild(li)
+                li.appendChild(a)
+                gkmenu.appendChild(li)
 
-      }
-
-    
-
+            }
 		});
 	
-    hex.addEventListener("click", function() {
-        if (!this.querySelector(".inside").classList.contains("active-hexagon")) {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        Activate(this)
-        rememberHexagon(this);
-          
-        } 			
-    });
+        hex.addEventListener("click", function() {
+            if (!this.querySelector(".inside").classList.contains("active-hexagon")) {
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+                Activate(this)
+                rememberHexagon(this);
+            } 			
+        });
   
-    hex.addEventListener("mouseover", function(event) {
+        hex.addEventListener("mouseover", function(event) {
 
-        //не убирать
-        let hexElement = event.target
+            //не убирать
+            let hexElement = event.target
 
-        //вывод в нижние окна
-        document.getElementById("outLT").value = getNGK(findHex(getMainHexFromSiblings(hexElement).id), 0).LT;
-        document.getElementById("outGK").value = getNGK(findHex(getMainHexFromSiblings(hexElement).id), 0).GK;
-        //--------------------------
+            //вывод в нижние окна
+            document.getElementById("outLT").value = getNGK(findHex(getMainHexFromSiblings(hexElement).id), 0).LT;
+            document.getElementById("outGK").value = getNGK(findHex(getMainHexFromSiblings(hexElement).id), 0).GK;
+            //--------------------------
 
-        let futureClickedHexagons = clickedHexagons.slice()
-        futureClickedHexagons.push(getMainHexFromSiblings(hexElement))
-        futureClickedHexagonsCoords = futureClickedHexagons.map(hexagon => getHexCanvasCoords(hexagon))
+            let futureClickedHexagons = clickedHexagons.slice()
+            futureClickedHexagons.push(getMainHexFromSiblings(hexElement))
+            futureClickedHexagonsCoords = futureClickedHexagons.map(hexagon => getHexCanvasCoords(hexagon))
 
-        if (drawingParallelogram) {
-        drawParallelogram(futureClickedHexagonsCoords,"yellow")
-        }
-      
-      			
-    });
-
-
+            if (drawingParallelogram) {
+                drawParallelogram(futureClickedHexagonsCoords,"yellow")
+            }		
+        });
 	}
 }
 
@@ -139,78 +124,70 @@ function sortGK(tableData) {
 }
 
 function showNotEmptyGK(row,hex) {
-  firstNonEmptyGK = findFirstNotEmptyNumber(data[row][hex])
-  firstGK = 0
-  swap = data[row][hex][firstGK]
-  data[row][hex][firstGK] = data[row][hex][firstNonEmptyGK]
-  data[row][hex][firstNonEmptyGK] = swap
+    firstNonEmptyGK = findFirstNotEmptyNumber(data[row][hex])
+    firstGK = 0
+    swap = data[row][hex][firstGK]
+    data[row][hex][firstGK] = data[row][hex][firstNonEmptyGK]
+    data[row][hex][firstNonEmptyGK] = swap
 }
 
 function findFirstNotEmptyNumber(hex) {
-  for (let gk in hex) {
-    if (getNGK(hex,gk).name != "") {
-      return gk
+    for (let gk in hex) {
+        if (getNGK(hex,gk).name != "") {
+            return gk
+        }
     }
-  }
-  return null
+    return null
 }
 
 function createTable(id,tableData) {
-  Ntable = document.getElementById(id)
-  Ntable.innerHTML = ""
+    Ntable = document.getElementById(id)
+    Ntable.innerHTML = ""
     for (let row in tableData) {
         if (row != 'laws') {
             Ntable.appendChild(createRow(tableData[row], row))
         }
     }
-  addHexEventListeners()
+    addHexEventListeners()
 }
 
 function createRow(rowData,rowName) {
-  newRow = document.createElement("div")
-  newRow.classList.add("row")
-  newRow.setAttribute("id",rowName)
-  for (let hex in rowData) {
-    //console.log(rowData[hex][0][Object.keys(rowData[hex][0])[0]])
-    newRow.appendChild(createHexagon(getNGK(rowData[hex],0),hex))
-  }
-  return newRow
+    newRow = document.createElement("div")
+    newRow.classList.add("row")
+    newRow.setAttribute("id",rowName)
+    for (let hex in rowData) {
+        newRow.appendChild(createHexagon(getNGK(rowData[hex],0),hex))
+    }
+    return newRow
 }
 
 
-
 function createHexagon(hexData,name) {
-  newHex = document.createElement("div")
-  newHex.setAttribute("data-level",hexData.color)
-  newHex.setAttribute("id",name)
-  newHex.classList.add("one-hexagon","menu")
+    newHex = document.createElement("div")
+    newHex.setAttribute("data-level",hexData.color)
+    newHex.setAttribute("id",name)
+    newHex.classList.add("one-hexagon","menu")
 
-  hexText = document.createElement("div")
-  hexText.classList.add("inside",hexData.color)
+    hexText = document.createElement("div")
+    hexText.classList.add("inside",hexData.color)
 
-  if (hexData.name == "") {
-    newHex.classList.add("invisible")
-  }
-  label = document.createElement("p")
-  label.innerHTML = `${hexData.name}`
-  hexText.appendChild(label)
-/*    if (hexData.ob_ed_izm != "" && hexData.usl_ob != "") {*/
-if (hexData.usl_ob != "") {
-    symbolMeasure = document.createElement("i")
-    if (hexData.M == 0 & hexData.L == 0 & hexData.T == 0 & hexData.I == 0 & hexData.name != "" & hexData.LT == 'L⁰T⁰') { symbolMeasure.innerHTML = '1' }
-    else { symbolMeasure.innerHTML = `${hexData.usl_ob.split(/[{}]/)[0]}${(hexData.usl_ob.split(/[{}]/)[1]) ? (hexData.usl_ob.split(/[{}]/)[1].fontsize(1)) : ''}${(hexData.usl_ob.split(/[{}]/)[2]) ? (hexData.usl_ob.split(/[{}]/)[2]) : ''}, ${hexData.ob_ed_izm}` }
-    hexText.appendChild(symbolMeasure)
-  }
-  mlt2 = document.createElement("b")
-  mlt2.innerHTML = getMLT(hexData)
-  hexText.appendChild(document.createElement("br"))
-  hexText.appendChild(mlt2)
-  newHex.appendChild(hexText)
+    if (hexData.name == "") {
+        newHex.classList.add("invisible")
+    }
+    label = document.createElement("p")
+    label.innerHTML = `${hexData.name}`
+    hexText.appendChild(label)
+    if (hexData.usl_ob != "") {
+        symbolMeasure = document.createElement("i")
+        if (hexData.M == 0 & hexData.L == 0 & hexData.T == 0 & hexData.I == 0 & hexData.name != "" & hexData.LT == 'L⁰T⁰') { symbolMeasure.innerHTML = '1' }
+        else { symbolMeasure.innerHTML = `${hexData.usl_ob.split(/[{}]/)[0]}${(hexData.usl_ob.split(/[{}]/)[1]) ? (hexData.usl_ob.split(/[{}]/)[1].fontsize(1)) : ''}${(hexData.usl_ob.split(/[{}]/)[2]) ? (hexData.usl_ob.split(/[{}]/)[2]) : ''}, ${hexData.ob_ed_izm}` }
+        hexText.appendChild(symbolMeasure)
+    }
+    mlt2 = document.createElement("b")
+    mlt2.innerHTML = getMLT(hexData)
+    hexText.appendChild(document.createElement("br"))
+    hexText.appendChild(mlt2)
+    newHex.appendChild(hexText)
 
-  return newHex
+    return newHex
 };
-
-
-
-
-
