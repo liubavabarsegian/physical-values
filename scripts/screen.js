69d8d3240ab5972652d14701a4e1501b4ecc634e
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-02-02 17:40:49 +0300

    screen

--- scripts/screen.js
+++ scripts/screen.js
@@ -2,8 +2,14 @@ function takeshot() {
     document.getElementById('output_screen').innerHTML = '';
 
     let node = document.getElementById('newf');
-    clickedHexagons.forEach(hexElement => Deactivate(hexElement));
-    clickedHexagons = [];
+    if (clickedHexagons.length > 0) {
+        clickedHexagons.forEach(hexElement => Deactivate(hexElement));
+        clickedHexagons = [];
+    }
+    if (currentShownLaw.hexes) {
+        hexagons = currentShownLaw.hexes.map(hex => document.getElementById(hex))
+        hexagons.forEach(hex => Deactivate(hex))
+    }
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     domtoimage.toPng(node)
         .then(dataUrl => {
